6b33ea951d1528b619cfa5ae4886443752cbebb4
ALAA HAMADA FOUAD <alaa111hamada@gmail.com>
2026-08-17 00:47:27 +0400

    clean: remove db files and temp scripts from tracking

--- check.cjs
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require("@prisma/client");
-const p = new PrismaClient();
-(async () => {
-  const r = await p.setting.findUnique({ where: { key: "store_description" } });
-  console.log("DB value:", JSON.stringify(r));
-  await p.$disconnect();
-})();
--- check.mjs
+++
@@ -1,1 +0,0 @@
-﻿import{PrismaClient}from"@prisma/client";const p=new PrismaClient();const r=await p.setting.findUnique({where:{key:"store_description"}});console.log(JSON.stringify(r));await p.$disconnect()
